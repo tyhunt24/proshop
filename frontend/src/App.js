38aa950391cd84e9a47cb6352c79188f867ce15e
Ty Hunt <jtmhunt22@gmail.com>
2020-10-05 11:33:57 -0600

    added Header and Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import {Container} from "react-bootstrap"
 import './App.css';
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 const App = () => {
   return (
-    <div className="App">
+    <>
+    <Header />
+    <main className="py-3">
+      <Container>
       <h1>Welcome to ProShop</h1>
-    </div>
+      </Container>
+     </main>
+    <Footer />
+    </>
   );
 }
 

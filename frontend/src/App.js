093edd9c340a846fc6da11c5fc6874bc563ee048
Ty Hunt <jtmhunt22@gmail.com>
2020-10-14 09:27:32 -0600

    user login and register completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import HomeScreen from "./screens/HomeScreen"
 import ProductScreen from "./screens/ProductScreen"
 import CartScreen from "./screens/CartScreen"
 import LoginScreen from "./screens/LoginScreen"
+import RegisterScreen from './screens/RegisterScreen';
 
 const App = () => {
   return (
@@ -19,6 +20,7 @@ const App = () => {
         <Route exact path="/product/:id" component={ProductScreen} />
         <Route exact path="/cart/:id?" component={CartScreen} />
         <Route exact path="/login" component={LoginScreen} />
+        <Route exact path="/register" component={RegisterScreen} />
       </Container>
      </main>
       <Footer />

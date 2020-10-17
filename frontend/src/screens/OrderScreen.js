1b1fd9d3dfd40707a92b6e8a6fceb4e8becc59d2
Ty Hunt <jtmhunt22@gmail.com>
2020-10-17 07:16:47 -0600

    starting payment method

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -2,7 +2,7 @@ import React, {useEffect} from 'react'
 import {Link} from "react-router-dom"
 import {Button, Row, Col, ListGroup, Image, Card} from "react-bootstrap"
 import {useDispatch, useSelector} from "react-redux"
-import Message from "../components/FormContainer"
+import Message from "../components/Message"
 import Loader from "../components/Loader"
 import {getOrderDetails} from "../actions/orderActions";
 
@@ -14,9 +14,20 @@ const OrderScreen = ({match}) => {
     const orderDetails = useSelector((state) => state.orderDetails)
     const {order, loading, error} = orderDetails
 
-    useEffect(() => {
+      //Calculate Prices
+      if (!loading) {
+        const addDecimals = (num) => {
+            return (Math.round(num * 100) / 100).toFixed(2)
+        }
+        order.itemsPrice = addDecimals(order.orderItems.reduce((acc, item) => 
+        acc + item.price * item.qty, 0))
+      }
+   
+   useEffect(() => {
+    if(!order || order._id !== orderId) {
         dispatch(getOrderDetails(orderId))
-    }, [])
+    }
+    }, [order, orderId])
 
 
     return loading ? <Loader /> : error ? <Message variant="danger">{error}
@@ -27,16 +38,25 @@ const OrderScreen = ({match}) => {
                     <ListGroup variant="flush">
                         <ListGroup.Item>
                             <h2>Shipping</h2>
+                           <p><strong>Name: </strong> {order.user.name}</p>
+                           <p><strong> Email: </strong> <a href={`mailto:${order.user.email}`}>
+                         {order.user.email}</a></p>
                             <p>
                                 <strong>Address: </strong>
                                 {order.shippingAddress.address}, {order.shippingAddress.city}, 
                                 {order.shippingAddress.postalCode}, {order.shippingAddress.country}
                             </p>
+                            {order.isDelivered ? <Message variant="success">Delivered on {order.deliveredAt}</Message> : 
+                         <Message variant="danger">Not Delivered</Message>}
                         </ListGroup.Item>
                         <ListGroup.Item>
                             <h2>Payment Method</h2>
+                            <p>
                             <strong>Method: </strong>
                             {order.paymentMethod}
+                            </p>
+                         {order.isPaid ? <Message variant="success">Paid on {order.paidAt}</Message> : 
+                         <Message variant="danger">Not Paid</Message>}
                         </ListGroup.Item>
                         <ListGroup.Item>
                             <h2>Order Items</h2>
--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -30,7 +30,7 @@ const PlaceOrderScreen = ({history}) => {
            history.push(`/order/${order._id}`)
        } 
          // eslint-disable-next-line
-    }, [history, success])
+    }, [history, success, order])
 
     const placeOrderHandler = (e) => {
         e.preventDefault()

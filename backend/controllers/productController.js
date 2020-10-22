2d400b4e342db9a6eea0a6612619ba17b81474ec
Ty Hunt <jtmhunt22@gmail.com>
2020-10-22 11:12:46 -0600

    backend review route complete

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,7 +4,7 @@ import Product from "../models/productModel.js"
 // @desc    fetch all products
 // @route    GET /api/products
 //@access    Public
-const getProducts = asyncHandler(async (req,res) => {
+const getProducts = asyncHandler(async (req, res) => {
     const products = await Product.find({})
     res.json(products)
 })
@@ -12,10 +12,10 @@ const getProducts = asyncHandler(async (req,res) => {
 // @desc    fetch single product
 // @route    GET /api/products/:id
 //@access    Public
-const getProductById = asyncHandler(async (req,res) => {
+const getProductById = asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id)
-    
-    if(product) {
+
+    if (product) {
         res.json(product)
     } else {
         res.status(404)
@@ -28,29 +28,37 @@ const getProductById = asyncHandler(async (req,res) => {
 //@access    Private/Admin
 const createProduct = asyncHandler(async (req, res) => {
     const product = new Product({
-      name: 'Sample name',
-      price: 0,
-      user: req.user._id,
-      image: '/images/sample.jpg',
-      brand: 'Sample brand',
-      category: 'Sample category',
-      countInStock: 0,
-      numReviews: 0,
-      description: 'Sample description',
+        name: 'Sample name',
+        price: 0,
+        user: req.user._id,
+        image: '/images/sample.jpg',
+        brand: 'Sample brand',
+        category: 'Sample category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Sample description',
     })
-  
+
     const createdProduct = await product.save()
     res.status(201).json(createdProduct)
-  })
+})
 
 // @desc     Update single product
 // @route    Put /api/products/:id
 //@access    Private/Admin
-const updateProduct = asyncHandler(async (req,res) => {
-    const {name, price, description, image, brand, category, countInStock} = req.body
+const updateProduct = asyncHandler(async (req, res) => {
+    const {
+        name,
+        price,
+        description,
+        image,
+        brand,
+        category,
+        countInStock
+    } = req.body
 
     const product = await Product.findById(req.params.id)
-    if(product) {
+    if (product) {
         product.name = name
         product.price = price
         product.description = description
@@ -67,4 +75,52 @@ const updateProduct = asyncHandler(async (req,res) => {
     }
 })
 
-export {getProducts, getProductById, createProduct, updateProduct}
+// @desc     Create new review
+// @route    GET /api/products/:id/reviews
+//@access    Public
+const createProductReview = asyncHandler(async (req, res) => {
+    const {
+        rating,
+        comment
+    } = req.body
+
+    const product = await Product.findById(req.params.id)
+
+    console.log(product)
+
+    if(product) {
+      const alreadyReviewed = product.reviews.find(r => r.user.ToString() === req.user._id.toString()) 
+      
+      if(alreadyReviewed) {
+          res.status(400)
+          throw new Error("Product already reviewed")
+      }
+
+      const review = {
+          name: req.user.name,
+          rating: Number(rating),
+          comment,
+          user: req.user._id
+      }
+
+      product.reviews.push(review)
+      product.numReviews = product.reviews.length
+      product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) 
+      / product.reviews.length
+      
+      await product.save()
+
+      res.status(201).json({message: "Review added"})
+    } else {
+        res.status(404)
+        throw new Error("Product not found")
+    }
+})
+
+export {
+    getProducts,
+    getProductById,
+    createProduct,
+    updateProduct,
+    createProductReview
+}
